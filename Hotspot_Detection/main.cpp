1d3ae17e9b77517a114b2fa91e767b17ca9275d5
Akash Sharma <aakashsharmajc100@gmail.com>
2019-11-05 22:33:51 +0530

    resolved

--- Hotspot_Detection/main.cpp
+++ Hotspot_Detection/main.cpp
@@ -65,11 +65,14 @@ bool liesincircle(double a,double b,int r,coord p){
   double rd=(p.x-a)*(p.x-a)+(p.y-b)*(p.y-b);
   return rd<=r*r;
 }
-vector<coord>  MECC(double x,double y, int radius,vector<coord> &p){
+pair<double,vector<coord> > MECC(double x,double y, int radius,vector<coord> &p){
   vector<coord> meccpoints;
+  map<pair<int,int>, int> mp;
   for(int m=0;m<p.size();m++){
     int i=p[m].x/lcell;
     int j=p[m].y/lcell;
+
+    mp[{i,j}]++;
 
     coord a=new coord(grid[i][j].Xmin,grid[i][j].Ymin);
     coord b=new coord(grid[i][j].Xmin,grid[i][j].Ymax);
@@ -80,15 +83,19 @@ vector<coord>  MECC(double x,double y, int radius,vector<coord> &p){
       meccpoints.push_back(p[m]);
     }
   }
-  //area to be calculated
-  return meccpoints;
+
+  double area=lcell*lcell*mp.size();
+  return {area,meccpoints};
 }
 
-vector<coord>  MFCC(double x,double y, int radius,vector<coord> &p){
+pair<double,vector<coord> >  MFCC(double x,double y, int radius,vector<coord> &p){
+  map<pair<int,int>, int> mp;
   vector<coord> mfccpoints;
   for(int m=0;m<p.size();m++){
     int i=p[m].x/lcell;
     int j=p[m].y/lcell;
+
+    mp[{i,j}]++;
 
     coord a=new coord(grid[i][j].Xmin,grid[i][j].Ymin);
     coord b=new coord(grid[i][j].Xmin,grid[i][j].Ymax);
@@ -99,8 +106,9 @@ vector<coord>  MFCC(double x,double y, int radius,vector<coord> &p){
       mfccpoints.push_back(p[m]);
     }
   }
-  //area to be calculated
-  return mfccpoints;
+
+  double area=lcell*lcell*mp.size();
+  return {area,mfccpoints};
 }
 double log_LRGrid_Upperbound(double areaMecc,double areaMfcc,int nMecc,int nMfcc){
   int uc=nMecc;
@@ -165,19 +173,26 @@ vector<pair<pair<int,int>,vector<coord> > > Filter_Phase(vector<int> pos,int the
 
           double cenx=(grid[i][j].Xmin+grid[i][j].Xmax)/2;
           double ceny=(grid[i][j].Ymin+grid[i][j].Ymax)/2;
-          vector<coord> cirmecc=MECC(cenx,ceny,r,pos);
-          int nMecc=cirmfcc.size();
-          //area to be chaged
-          double areaMecc=PI*r*r;
-          vector<coord> cirmfcc=MFCC(cenx,ceny,r,pos);
+          double areaMecc,areaMfcc;
+          vector<coord> cirmecc,cirmfcc;
+
+          pair<double,vector<coord> > p1=MECC(cenx,ceny,r,pos);
+          pair<double,vector<coord> > p2=MFCC(cenx,ceny,r,pos);
+
+          areamecc=p1.first;
+          areamfcc=p2.first;
+          cirmecc=p1.second;
+          cirmfcc=p2.second;
+          int nMfecc=cirmecc.size();
           int nMfcc=cirmfcc.size();
-          //area to be chaged
-          double areaMfcc=PI*r*r;
+          
+          // if we take area as area of circle double areaMecc=PI*r*r;
+          //if we take area as area of circle double areaMfcc=PI*r*r;
+
           double llr=log_LRGrid_Upperbound(nMecc,nMfcc,areaMecc,areaMfcc);
           if(llr>=thetha && llr>maxLLR){
               maxLLr=llr;
-          //to be chaged x and y
-              maxxX=cenx;maxxY=ceny;maxxR=r;xx=x;yy=y;
+              maxxX=cenx;maxxY=ceny;maxxR=r;xx=i;yy=j;
           }
         }
       }
@@ -205,13 +220,13 @@ circle SEC(vector<coord> p, int i,vector<coord>r){
 
 }
 vector<circles> Refine_Phase(vector<pair<pair<int,int>,vector<coord> > > fset,int thetha,int rmin){
+
   vector<circles> candidate_circles;
   for(int i=0;i<fset.size();i++){
     pair<int,int> cellcenter=fset[i].first;
     int r=inf;
-    //kuch to chod h
-    double cenx=(grid[i][j].Xmin+grid[i][j].Xmax)/2;
-    double ceny=(grid[i][j].Ymin+grid[i][j].Ym  ax)/2;
+    double cenx=(grid[cellcenter.x][cellcenter.y].Xmin+grid[cellcenter.x][cellcenter.y].Xmax)/2;
+    double ceny=(grid[cellcenter.x][cellcenter.y].Ymin+grid[cellcenter.x][cellcenter.y].Ymax)/2;
     circle maxC;
     double maxllr=-1;
     
@@ -298,7 +313,8 @@ void generatemontecarlo(int m,double montecarlo[5000],double areaS)
 int main()
 {
     //INPUTS
-    int thetha,rmin,alphaP,mSim_m;
+    double thetha,rmin,alphaP;
+    int mSim_m;
     thetha=0;
     rimn=2;
     alphaP=0.1;
@@ -316,8 +332,8 @@ int main()
 
     //GRID VARIABLES
     
-          //ceil to be chaged
-    double lcell=rmin/2;
+    //ceil to be chaged
+    double lcell=ceil(rmin/2);
     int N=sidelength/lcell;
     int total_countGrid_cells=N*N;
     total_cubicGrid_cells=N*N*N;
